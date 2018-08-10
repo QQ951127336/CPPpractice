3f3960b98f1b338e435f80d16061d8f5de197382
unknown <951127336@qq.com>
2018-08-10 14:24:11 +0800

    'pentagonal2'

---
+++ Test/pentagonal2.cpp
@@ -0,0 +1,28 @@
+#include"stdafx.h"
+#include<iostream>
+#include<vector>
+using namespace std;
+
+inline bool check_validity(int pos) {
+	return (pos < 0 || pos > 64) ? false : true;
+}
+const vector<int>*
+pentagonal_series(int pos) {
+	static	vector<int> _elems;
+	if (check_validity(pos) && (pos > _elems.size())) {
+		for (int ix = _elems.size() + 1; ix <= pos; ix++) {
+			_elems.push_back((ix*(3 * ix)) / 2);
+		}
+		return &_elems;
+	}
+}
+bool pentagonal_elem(int pos, int &elem) {
+	if (!check_validity(pos)) {
+		cout << "Sorry. Invalid position: " << pos << endl;
+		elem = 0;
+		return false;
+	}
+	const vector<int> *pent = pentagonal_series(pos);
+	elem = (*pent)[pos-1];
+	return true;
+}
