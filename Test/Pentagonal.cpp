80f04b2e000a4e7f1f6c3dc421e50d2d422c104a
unknown <951127336@qq.com>
2018-08-10 14:10:52 +0800

    'pentagonal'

--- Test/Pentagonal.cpp
+++ Test/Pentagonal.cpp
@@ -4,16 +4,16 @@
 #include<iostream>
 using namespace std;
 
-//bool calc_elements(vector<int>&vec, int pos) {
-//	int pn = pos*(3 * pos - 1) / 2;
-//	vec.push_back(pn);
-//	return true;
-//}
-//
-//void display(vector<int> vec, const string title, ostream &os = cout) {
-//	os <<title;
-//	for (int i = 0; i < vec.size(); i++) {
-//		os << " "<<vec[i] ;
-//	}
-//	os << endl;
-//}
+bool calc_elements(vector<int>&vec, int pos) {
+	int pn = pos*(3 * pos - 1) / 2;
+	vec.push_back(pn);
+	return true;
+}
+
+void display(vector<int> vec, const string title, ostream &os = cout) {
+	os <<title;
+	for (int i = 0; i < vec.size(); i++) {
+		os << " "<<vec[i] ;
+	}
+	os << endl;
+}
